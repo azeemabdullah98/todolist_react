ccc8f1453cb1e8e9d4c0996d7fe351c9bf3e8b21
azeemabdullah98 <azeemquest98@gmail.com>
2024-06-01 17:32:23 +0530

    restructure index.js file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import App from "./src/components/App";
 
 const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element);
